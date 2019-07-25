bcad58dc6fc5c5359a914ab893a5672e93680017
_Pythagoras <andreas.chatzivasileiadis@gmail.com>
2019-07-25 18:49:18 +0300

    Add navigation to menu-item component using withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import "./App.css";
 
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return <div>
-    <HomePage />
+    <Switch>
+      <Route path='/hats' component={HatsPage} />
+      <Route path='/' component={HomePage} />
+    </Switch>
+    {/* <HomePage /> */}
   </div>;
 }
 
