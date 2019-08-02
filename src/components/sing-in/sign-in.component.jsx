8d7ddf3ad3fc338f6d7b8d3deafff0ff57747637
_Pythagoras <andreas.chatzivasileiadis@gmail.com>
2019-08-02 17:45:16 +0300

    Finish sign in handleSubmit method

--- src/components/sing-in/sign-in.component.jsx
+++ src/components/sing-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
@@ -16,10 +16,17 @@ class SingIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch(error) {
+      console.error(error);
+    }
   };
 
   handleChange = event => {
